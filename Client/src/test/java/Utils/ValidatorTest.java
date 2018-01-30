c84588e7392ae014ddcde37fdb7fdfc3570a0ec5
Jakub Tomczak <jakub.tomczak@live.com>
2018-01-30 22:19:20 +0100

    Update tests.

--- Client/src/test/java/Utils/ValidatorTest.java
+++ Client/src/test/java/Utils/ValidatorTest.java
@@ -9,7 +9,7 @@ public class ValidatorTest {
     public void validateTime() throws Exception {
         assertTrue(Validator.validateTime("12:34"));
         assertTrue(Validator.validateTime("00:00"));
-
+        assertFalse(Validator.validateTime(""));
         assertFalse(Validator.validateTime("asdczxczxc"));
         assertFalse(Validator.validateTime("aa:34"));
         assertFalse(Validator.validateTime("43:34"));
@@ -19,6 +19,7 @@ public class ValidatorTest {
 
     @Test
     public void validateString() throws Exception {
+        assertFalse(Validator.validateString(""));
         assertTrue(Validator.validateString("asdczxczxc"));
         assertTrue(Validator.validateString("AbrakadabraA"));
 
@@ -27,6 +28,18 @@ public class ValidatorTest {
         assertFalse(Validator.validateString("asdasd!@#asdas"));
         assertFalse(Validator.validateString("123asdasd123asdas123"));
         assertFalse(Validator.validateString("1111asdasdsdas"));
+    }
+
+    @Test
+    public void validateNumber() {
+        assertTrue(Validator.validateNumber("3.12"));
+        assertTrue(Validator.validateNumber("33.12"));
+
+        assertFalse(Validator.validateNumber("asdasd123asdas"));
+        assertFalse(Validator.validateNumber("asdasdsdas111"));
+        assertFalse(Validator.validateNumber("3.12jhgjh"));
+        assertFalse(Validator.validateNumber(""));
+
     }
 
 }
